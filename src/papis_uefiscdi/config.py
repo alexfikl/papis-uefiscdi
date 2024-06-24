20c4475c34be1e0135fbef5ab76e89d22e5fc1d0
Alexandru Fikl <alexfikl@gmail.com>
2024-06-24 21:42:03 +0300

    feat: add links to previous years

--- src/papis_uefiscdi/config.py
+++ src/papis_uefiscdi/config.py
@@ -33,6 +33,8 @@ UEFISCDI_DATABASE_TO_KEY = {
 }
 """A mapping of database identifier to unique key names."""
 
+# NOTE: This mostly has the last 5 years, since those are required for UEFISCDI
+# competitions.
 UEFISCDI_DATABASE_URL = {
     2023: {
         "aisq": "https://uefiscdi.gov.ro/resource-866007-zone.iunie.2023.ais.pdf",
@@ -40,7 +42,35 @@ UEFISCDI_DATABASE_URL = {
         "ais": "https://uefiscdi.gov.ro/resource-863884-ais_2022.xlsx",
         "ris": "https://uefiscdi.gov.ro/resource-863882-ris_2022.xlsx",
         "rif": "https://uefiscdi.gov.ro/resource-863887-rif_2022.xlsx",
-    }
+    },
+    2022: {
+        "aisq": "https://uefiscdi.gov.ro/resource-862159-zone.2022.ais.pdf",
+        "jifq": "https://uefiscdi.gov.ro/resource-862151-zone.2022.if.pdf",
+        "ais": "https://uefiscdi.gov.ro/resource-862108-ais.2021.xlsx",
+        "ris": "https://uefiscdi.gov.ro/resource-862102-ris.2021.xlsx",
+        "rif": "https://uefiscdi.gov.ro/resource-862155-rif.2021.xlsx",
+    },
+    2021: {
+        "aisq": "https://uefiscdi.gov.ro/resource-820923-ais2021.pdf",
+        "jifq": "https://uefiscdi.gov.ro/resource-820921-if2021.pdf",
+        "ais": "https://uefiscdi.gov.ro/resource-820980-ais.2020.xlsx",
+        "ris": "https://uefiscdi.gov.ro/resource-820984-sri.2020.xlsx",
+        "rif": "https://uefiscdi.gov.ro/resource-820987-rif.2020.xlsx",
+    },
+    2020: {
+        "aisq": "https://uefiscdi.gov.ro/resource-821878-clasament2020.ais.pdf",
+        "jifq": "https://uefiscdi.gov.ro/resource-821873-clasament2020.if.pdf",
+        "ais": "https://uefiscdi.gov.ro/resource-821312-ais2019-iunie2020-.valori.cuartile.xlsx",
+        "ris": "https://uefiscdi.gov.ro/resource-829001-sri.2019.xlsx",
+        "rif": "https://uefiscdi.gov.ro/resource-829003-rif.2019.xlsx",
+    },
+    2019: {
+        "aisq": "https://uefiscdi.gov.ro/resource-822841",
+        "jifq": "https://uefiscdi.gov.ro/resource-822843",
+        "ais": "https://uefiscdi.gov.ro/resource-828068",
+        "ris": "https://uefiscdi.gov.ro/resource-828022",
+        "rif": "https://uefiscdi.gov.ro/resource-828027",
+    },
 }
 """A mapping of database identifiers to URLs containing the databases themselves.
 """
